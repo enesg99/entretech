b14d9d753a35887cba1f10bc2082c56b26939cce
Enes <enesgurleyen@hotmail.de>
2024-10-13 03:09:27 +0200

    Added first content on the Webdev Basic Page

--- src/pages/Webdev/Basics.js
+++ src/pages/Webdev/Basics.js
@@ -1,21 +1,78 @@
 import Sidebar from "../../components/sidebarWebdev";
+import Footer from "../../components/footer"
 import "./WebdevStyle/Webdev.css";
+import laptop from "../../images/laptop.png"
+import network from "../../images/network.png"
+import ai from "../../images/ai.png"
+import programming from "../../images/programmingLang.png"
+import programmer from "../../images/programmer.png"
 
 function Basics() {
     return ( 
         <>
             <div className="webdevContainer">
                 <h1>Basics</h1>
-                {/* <h2>Topics</h2>
-                <ol>
-                    <li>Was ist ein Computer und wie funkioniert er?</li>
-                    <li>Was ist das Internet?</li>
-                    <li>Was ist Programmieren?</li>
-                </ol> */}
-                <h2>Was ist ein Computer?</h2>
-                <p>Einfach ausgedrückt ist ein Computer ein Gerät, welches </p>
+                <div className="contentContainer">
+                    <div className="textContent">
+                        <h2>Computer</h2>
+                        <p>
+                            Ein Computer ist eine Maschine, die ohne Anweisungen nichts von alleine machen kann. 
+                            Einfach gesagt, er verarbeitet Daten, führt Berechnungen durch, speichert Informationen 
+                            und gibt diese bei Bedarf aus.
+                        </p>
+                    </div>
+                    <img src={laptop} alt="Laptop" />
+                </div>
+                <div className="contentContainer">
+                    <div className="textContent">
+                        <h2>Internet</h2>
+                        <p>
+                            Das Internet ist ein riesiges Netzwerk von Geräten, die weltweit miteinander
+                            verbunden sind, um Informationen auszutauschen. Diese Geräte können Computer,
+                            Smartphones, Server und viele andere sein.
+                        </p>
+                    </div>
+                    <img src={network} alt="Network" />
+                </div>
+                <div className="contentContainer">
+                    <div className="textContent">
+                        <h2>Programmieren</h2>
+                        <p>
+                            Programmieren bedeutet, Anweisungen für den Computer oder andere Geräte zu schreiben.
+                            Diese Anweisungen sind in Programmiersprachen verfasst, die der Mensch lesen kann.
+                            Damit der Computer diese Anweisungen ausführen kann, werden sie von speziellen Programmen,
+                            sogenannten Compilern oder Interpretern, in eine für den Computer verständliche Sprache übersetzt.
+                        </p>
+                    </div>
+                    <img src={ai} alt="AI" />
+                </div>
+                <div className="contentContainer">
+                    <div className="textContent">
+                        <h2>Programmiersprachen</h2>
+                        <p>
+                            Es gibt viele verschiedene Programmiersprachen, weil sie für unterschiedliche Zwecke entwickelt 
+                            wurden und unterschiedliche Eigenschaften besitzen. Einige Programmiersprachen eignen sich 
+                            besonders gut für die Erstellung von Websites, andere sind für die Steuerung von Hardware oder 
+                            die Entwicklung von Spielen optimiert. Zudem unterscheidet man zwischen höheren und niedrigeren 
+                            Programmiersprachen. Höhere Programmiersprachen sind menschenlesbarer und einfacher zu verstehen, 
+                            während niedrigere Programmiersprachen näher an der Maschinensprache sind und häufig zur direkten 
+                            Steuerung von Hardware verwendet werden.
+                        </p>
+                    </div>
+                    <img src={programming} alt="Programming languages" />
+                </div>
+                <div className="contentContainer">
+                    <div className="textContent">
+                        <h2>Web Entwicklung und dieser Track</h2>
+                        <p>
+                            
+                        </p>
+                    </div>
+                    <img src={programmer} alt="Programmer" />
+                </div>
             </div>
             <Sidebar />
+            <Footer />
         </>
     );
 }
